8ba1bbf5f8fdd87b8636cc363eed5a85066c53fa
WonJunKim <kimjnsjwj@gmail.com>
2018-06-02 16:44:46 +0900

    Repo Init

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 12345;
 
 express()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended : true}))
     .get('/', (req, res) => res.send('<h1>Hello, World!</h1>'))
-    .listen(PORT, () => console.log('Listening at ' + PORT));
+    .listen(PORT, () => console.log(`Listening at ${PORT}`));
