76531ed12aa53731c692b1252dd8b9b2f3b0a635
WonJunKim <kimjnsjwj@gmail.com>
2018-06-03 07:01:39 +0900

    delete defaultPic

--- models/tip.js
+++ models/tip.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    pic: {type: String, default: 'https://spirit-in-motion.herokuapp.com/tip/defaultPic'},
+    pic: {type: String, default: 'https://image.flaticon.com/icons/svg/34/34322.svg'},
     title: String,
     writer: String,
     content: String
--- routes/tip/index.js
+++ routes/tip/index.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const ctrl = require('./tip.ctrl');
 
 router.get('/', ctrl.getTips);
-router.use('/defaultPic', express.static('../../public/seo-tips-interface-symbol.png'));
 
 module.exports = router;
