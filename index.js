f48a448b1dde119275b023da98693689e66c90b5
WonJunKim <kimjnsjwj@gmail.com>
2018-06-02 16:34:15 +0900

    add Procfile

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require('express');
 const PORT = process.env.PORT | 3000;
 
 express()
-    .get('/', (req, res) => res.send('hello'))
+    .get('/', (req, res) => res.send('<h1>Hello, World!</h1>'))
     .listen(PORT, () => console.log('Listening at ' + PORT));
