ee25595a75db63a97a8b6dcde22ba70ce6849d5e
WonJunKim <kimjnsjwj@gmail.com>
2018-06-03 00:53:17 +0900

    debug

--- routes/place/index.js
+++ routes/place/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require('./placeSerach.ctrl');
+const ctrl = require('./place.ctrl');
 
 router.get('/', ctrl.getPlaces);
 router.post('/', ctrl.addPlace);
