effd857097116725ea958d52c47e4b25c897513e
WonJunKim <kimjnsjwj@gmail.com>
2018-06-02 20:36:27 +0900

    delete sendStatus

--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -5,16 +5,21 @@ function signIn(req, res) {
         id: req.body.id
     }, (err, user) => {
         if (err) {
-            res.sendStatus(500);
+            res.status(500).json({
+                'result': 'failure'
+            });
         } else if (user === null) {
-            res.sendStatus(405);
+            res.status(405).json({
+                'result': 'failure'
+            });
         } else if (user.pw === req.body.pw) {
             res.status(200).json({
-                name: user.name,
-                phoneNum: user.phoneNum
-            });
+                'result': 'success'
+            })
         } else {
-            res.sendStatus(405);
+            res.status(405).json({
+                'result': 'failure'
+            });
         }
     });
 }
@@ -24,26 +29,31 @@ function signUp(req, res) {
 
     console.log(req.body);
 
-    newUser.name = req.body.name;
-    newUser.id = req.body.id;
-    newUser.pw = req.body.pw;
-
+    newUser = req.body;
     Users.findOne({
         id: req.body.id
     }, (err, user) => {
         if (err) {
-            res.sendStatus(500);
+            res.status(500).json({
+                'result': 'failure'
+            });
         } else if (user === null) {
             newUser.save((err) => {
                 if (err) {
-                    res.sendStatus(500);
+                    res.status(500).json({
+                        'result': 'failure'
+                    });
                     return;
                 } else {
-                    res.sendStatus(200);
+                    res.status(200).json({
+                        'result': 'success'
+                    });
                 }
             });
         } else {
-            res.sendStatus(405);
+            res.status(405).json({
+                'result': 'failure'
+            });
         }
     });
 }
